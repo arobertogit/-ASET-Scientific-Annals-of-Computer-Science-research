5421da499b6a3bec6ad48dd938d0bab1eab169a5
cornel1923 <cornel1923@yahoo.com>
2014-11-02 17:30:06 +0200

    Design Patters [Singleton & Observer-> Models]

---
+++ Implementation/TAIP/TAIP/Models/Design Patterns/ReadJournal[Singleton].cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TAIP.Models.Design_Patterns
+{
+    public class ReadJournal_Singleton_
+    {
+        public class ReadJournal
+        {
+            private static ReadJournal self;
+            private string name;
+
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+
+            //hide the default constructor
+            private ReadJournal() { }
+
+            public static ReadJournal Instance()
+            {
+                //create instance only if we don't have one
+                if (self == null)
+                    self = new ReadJournal();
+                return self;
+            }
+        }
+    }
+}
---
+++ Implementation/TAIP/TAIP/Models/Design Patterns/Subscribe[Observer].cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TAIP.Models.Design_Patterns
+{
+    public class Subscribe_Observer_
+    {
+    }
+
+    public class Subscribe : ISubscribe
+    {
+        private List<Observer> observers = new List<Observer>();
+        private int _int;
+        public int Inventory
+        {
+            get
+            {
+                return _int;
+            }
+            set
+            {
+                // Just to make sure that if there is an increase in inventory then only we are notifying the observers.
+                if (value > _int)
+                    Notify();
+                _int = value;
+            }
+        }
+        public void SubscribeUser(Observer observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void UnsubscribeUser(Observer observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            observers.ForEach(x => x.Update());
+        }
+    }
+
+
+    public class Observer : IObserver
+    {
+        string ObserverEmail { get; private set; }
+        public Observer(string name)
+        {
+            this.ObserverEmail = name;
+        }
+        public void Update()
+        {
+            Console.WriteLine("{0}: A paper new paper was published.", this.ObserverEmail);
+        }
+    }
+
+    interface ISubscribe
+    {
+        void Subscribe(Observer observer);
+        void Unsubscribe(Observer observer);
+        void Notify();
+    }
+    interface IObserver
+    {
+        void Update();
+    }
+}
